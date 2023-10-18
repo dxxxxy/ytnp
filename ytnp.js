2e2c114f3bc69b81ffdc4afe5126384b77bc1ad7
dxxxxy <introsdream@gmail.com>
2023-10-18 15:14:10 -0400

    feat: update stuff

--- ytnp.js
+++ ytnp.js
@@ -1,14 +1,17 @@
 console.log("loaded. dxxxxy was here.")
 
 new MutationObserver((mutationList, _observer) => {
-   const popup = document.querySelector("body > ytd-app > ytd-popup-container > tp-yt-paper-dialog > ytd-enforcement-message-view-model").parentNode
+   try {
+      //get the popup element ONLY if it is related to adblock
+      const popup = document.querySelector("body > ytd-app > ytd-popup-container > tp-yt-paper-dialog > ytd-enforcement-message-view-model").parentNode
 
-   if (popup) {
-      //remove popup
-      popup.remove()
-      console.log("removed.")
+      if (popup) {
+         //remove popup
+         popup.remove()
+         console.log("removed.")
 
-      //resume video
-      document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button").click()
-   }
+         //resume video
+         document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button").click()
+      }
+   } catch {} //disregard errors
 }).observe(document.querySelector("body > ytd-app > ytd-popup-container"), { childList: true, subtree: true })
