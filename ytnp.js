c83434f998e96453215bb0021176761347e4e2f4
dxxxxy <introsdream@gmail.com>
2023-10-18 15:33:27 -0400

    feat: retry observing

--- ytnp.js
+++ ytnp.js
@@ -1,17 +1,27 @@
-console.log("loaded. dxxxxy was here.")
+const tick = setInterval(() => {
+   console.log("trying to observe.")
 
-new MutationObserver((mutationList, _observer) => {
    try {
-      //get the popup element ONLY if it is related to adblock
-      const popup = document.querySelector("body > ytd-app > ytd-popup-container > tp-yt-paper-dialog > ytd-enforcement-message-view-model").parentNode
+      //observe popup container element
+      new MutationObserver((mutationList, _observer) => {
+         try {
+            //get the popup element ONLY if it is related to adblock
+            const popup = document.querySelector("body > ytd-app > ytd-popup-container > tp-yt-paper-dialog > ytd-enforcement-message-view-model").parentNode
+   
+            if (popup) {
+               //remove popup
+               popup.remove()
+               console.log("removed.")
+   
+               //resume video
+               document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button").click()
+            }
+         } catch {} //disregard errors
+      }).observe(document.querySelector("body > ytd-app > ytd-popup-container"), { childList: true, subtree: true })
 
-      if (popup) {
-         //remove popup
-         popup.remove()
-         console.log("removed.")
-
-         //resume video
-         document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button").click()
-      }
-   } catch {} //disregard errors
-}).observe(document.querySelector("body > ytd-app > ytd-popup-container"), { childList: true, subtree: true })
+      clearInterval(tick)
+      console.log("observing. dxxxxy was here.")
+   } catch {
+      console.log("failed to observe. will retry.")
+   }
+}, 1000)
