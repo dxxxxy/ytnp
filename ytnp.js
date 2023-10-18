93d947933b2fd9ccf5753ce2f8cf495b6ed0b2bd
dxxxxy <introsdream@gmail.com>
2023-10-18 15:02:36 -0400

    feat: resume video after popup removal

--- ytnp.js
+++ ytnp.js
@@ -4,7 +4,11 @@ new MutationObserver((mutationList, _observer) => {
    const popup = document.querySelector("body > ytd-app > ytd-popup-container > tp-yt-paper-dialog > ytd-enforcement-message-view-model").parentNode
 
    if (popup) {
+      //remove popup
       popup.remove()
       console.log("removed.")
+
+      //resume video
+      document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button").click()
    }
 }).observe(document.querySelector("body > ytd-app > ytd-popup-container"), { childList: true, subtree: true })
